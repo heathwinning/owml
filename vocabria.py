16c86894ffb287c0fa76d083c3eebffea4b335e7
Heath Winning <heathwinning@gmail.com>
2020-03-17 09:38:43 +1100

    initial commit

---
+++ vocabria.py
@@ -0,0 +1,134 @@
+from google.cloud import texttospeech
+from google.cloud import translate
+import os
+import babel
+import numpy as np
+from moviepy.editor import *
+from moviepy.video.tools.segmenting import findObjects
+import youtube_upload
+import argparse
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/Users/heath/projects/vocabria/vocabria.json"
+
+def speak_in_many_languages(text, privacy_status=youtube_upload.PrivacyStatus.PUBLIC):
+    text = text.title()
+    directory = os.path.join('videos', text)
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+    video_snippets = []
+    languages = ['en'] + spoken_languages()
+    #languages = languages[:1]
+    for index, language in enumerate(languages):
+        translation, audio_output_path = translate_and_speak(text, language, directory)
+        video_snippets.append(language_video_snippet(text, translation, language, audio_output_path, index==len(languages)))
+    video_snippets.append(closing_message())
+    full_video = concatenate_videoclips(video_snippets)
+    video_path = os.path.join(directory, f'{text}.mp4')
+    full_video.write_videofile(video_path,fps=24,codec='mpeg4')
+    language_names = [language_name(language, native=False) for language in languages]
+    youtube_upload.upload_to_youtube(video_path, f'"{text}" spoken in many languages', f'Hear the word "{text}" spoken in many different languages.\n\n{", ".join(language_names)}', category=27, keywords=['language', 'translation', 'pronunciation', text], privacy_status=privacy_status)
+
+def language_name(language, native=True):
+    if native:
+        return babel.Locale.parse(language).get_display_name(language)
+    else:
+        return babel.Locale.parse(language).get_display_name('en')
+
+project_id = 'vocabria'
+translate_client = translate.TranslationServiceClient()
+client_parent = translate_client.location_path(project_id, 'global')
+def translate(text, language_code):
+    if language_code == 'en':
+        return text
+    response = translate_client.translate_text(
+        parent=client_parent,
+        contents=[text],
+        source_language_code="en-US",
+        target_language_code=language_code
+    )
+    return response.translations[0].translated_text
+
+texttospeech_client = texttospeech.TextToSpeechClient()
+def speak(text, language, filename):
+    synthesis_input = texttospeech.types.SynthesisInput(text=text)
+    voice_selection = texttospeech.types.VoiceSelectionParams(
+        language_code=language
+    )
+    audio_config = texttospeech.types.AudioConfig(
+        audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+    response = texttospeech_client.synthesize_speech(synthesis_input, voice_selection, audio_config)
+    with open(filename, 'wb') as out:
+        out.write(response.audio_content)
+        print(f'Audio content written to file "{filename}"')
+
+def translate_and_speak(text, language, out_dir):
+    translation = translate(text, language)
+    audio_filename = f'{text}_{language}.mp3'
+    path = os.path.join(out_dir, audio_filename)
+    speak(translation, language, path)
+    return translation, path
+
+def spoken_languages():
+    voices = texttospeech_client.list_voices().voices
+    unique_languages = []
+    for voice in voices:
+        language_code = voice.language_codes[0].split('-')[0]
+        if language_code not in unique_languages and not language_code == 'en':
+            unique_languages.append(language_code)
+    return unique_languages
+
+screensize = (720,460)
+def language_video_snippet(en_text, translated_text, language, audio_path, fade_en_text=False):
+    language_native = language_name(language, native=True)
+    language_english = language_name(language, native=False)
+
+    background_image_clip = ImageClip("background-image.png")
+    audio_clip = AudioFileClip(audio_path)
+
+    animation_duration = 0.7
+    wait_duration = 0.5
+
+    translated_text_clip = TextClip(translated_text, color='black', font="ArialUnicode", fontsize=200).set_position(('center', 0.47), relative=True)
+    en_text_clip = TextClip(en_text, color='black', font='ArialUnicode', fontsize=200).set_position(('center', 0.2), relative=True)
+    language_text_clip = TextClip(f'{language_native} ({language_english})', color='black', font='ArialUnicode', fontsize=100).set_position(('center', 0.6), relative=True)
+
+    before_audio_video_clip = CompositeVideoClip([
+        background_image_clip.subclip(0, animation_duration+wait_duration),
+        en_text_clip.subclip(0, animation_duration+wait_duration),
+        translated_text_clip.subclip(0, animation_duration).crossfadein(animation_duration),
+        translated_text_clip.subclip(0, wait_duration).set_start(animation_duration),
+        language_text_clip.subclip(0, animation_duration).crossfadein(animation_duration),
+        language_text_clip.subclip(0, wait_duration).set_start(animation_duration)
+        ])
+    audio_video_clip = CompositeVideoClip([
+        background_image_clip.subclip(0, audio_clip.duration),
+        en_text_clip.subclip(0, audio_clip.duration),
+        translated_text_clip.subclip(0, audio_clip.duration),
+        language_text_clip.subclip(0, audio_clip.duration)
+        ]).set_audio(audio_clip)
+    after_audio_video_clip = CompositeVideoClip([
+        background_image_clip.subclip(0, wait_duration+animation_duration),
+        *([en_text_clip.subclip(0, animation_duration+wait_duration)] if not fade_en_text else [
+            en_text_clip.subclip(0, wait_duration),
+            en_text_clip.subclip(0, animation_duration).crossfadeout(animation_duration).set_start(wait_duration),
+        ]),
+        translated_text_clip.subclip(0, wait_duration),
+        translated_text_clip.subclip(0, animation_duration).crossfadeout(animation_duration).set_start(wait_duration),
+        language_text_clip.subclip(0, wait_duration),
+        language_text_clip.subclip(0, animation_duration).crossfadeout(animation_duration).set_start(wait_duration)
+        ])
+
+    return concatenate_videoclips([
+        before_audio_video_clip,
+        audio_video_clip,
+        after_audio_video_clip
+        ])
+    
+def closing_message():
+    duration = 4
+    background_image_clip = ImageClip("images/rosetta-parchment-layers.png")
+    message_clip = TextClip("""Leave a comment with words\nyou would like to hear translated!""", color='black', font="ArialUnicode", fontsize=150).set_position(('center', 0.35), relative=True)
+    return CompositeVideoClip([
+        background_image_clip.subclip(0,duration),
+        message_clip.subclip(0,duration).crossfadein(0.7)
+    ])
---
+++ youtube_upload.py
@@ -0,0 +1,139 @@
+import httplib2
+import os
+import random
+import time
+import datetime
+from enum import Enum
+import json
+
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from apiclient.http import MediaFileUpload
+
+from google_auth_oauthlib.flow import InstalledAppFlow
+import google.oauth2
+
+httplib2.RETRIES = 1
+
+MAX_RETRIES = 10
+
+RETRIABLE_EXCEPTIONS = (httplib2.HttpLib2Error, IOError)
+
+RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
+
+CLIENT_SECRETS_FILE = "client_secrets.json"
+SAVED_CREDENTIALS_FILE = 'credentials.json'
+
+YOUTUBE_UPLOAD_SCOPE = "https://www.googleapis.com/auth/youtube.upload"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+class PrivacyStatus(Enum):
+  PUBLIC = 'public'
+  PRIVATE = 'private'
+  UNLISTED = 'unlisted'
+  def __str__(self):
+    return self.value
+
+def get_authenticated_service():
+  flow = InstalledAppFlow.from_client_secrets_file(
+      CLIENT_SECRETS_FILE,
+      scopes=[YOUTUBE_UPLOAD_SCOPE])
+
+  credentials = None
+  if os.path.exists(SAVED_CREDENTIALS_FILE):
+    info = None
+    with open(SAVED_CREDENTIALS_FILE, 'r') as f:
+      info = json.load(f)
+    credentials = google.oauth2.credentials.Credentials(
+      token=info['token'],
+      refresh_token=info['refresh_token'],
+      token_uri=info['token_uri'],
+      client_id=info['client_id'],
+      client_secret=info['client_secret'],
+      scopes=info['scopes'],
+    )
+    credentials.expiry = datetime.datetime.fromisoformat(info['expiry'])
+    if credentials.expired:
+      credentials.refresh(google.auth.transport.requests.Request())
+  else:
+    flow.run_local_server()
+    credentials = flow.credentials
+
+    info = { 
+      'token': credentials.token,
+      'refresh_token': credentials.refresh_token,
+      'token_uri': credentials.token_uri,
+      'client_id': credentials.client_id,
+      'client_secret': credentials.client_secret,
+      'scopes': credentials.scopes,
+      'expiry': credentials.expiry.isoformat(),
+    }
+    with open(SAVED_CREDENTIALS_FILE, 'w') as f:
+      json.dump(info, f)
+
+  return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, credentials=credentials)
+
+def initialize_upload(youtube, filename, title, description, category, keywords, privacy_status):
+
+  body=dict(
+    snippet=dict(
+      title=title,
+      description=description,
+      tags=keywords,
+      categoryId=category
+    ),
+    status=dict(
+      privacyStatus=privacy_status.value,
+      selfDeclaredMadeForKids=False
+    )
+  )
+  insert_request = youtube.videos().insert(
+    part=",".join(body.keys()),
+    body=body,
+    media_body=MediaFileUpload(filename, chunksize=-1, resumable=True)
+  )
+
+  resumable_upload(insert_request)
+
+def resumable_upload(insert_request):
+  response = None
+  error = None
+  retry = 0
+  while response is None:
+    try:
+      print("Uploading file...")
+      status, response = insert_request.next_chunk()
+      if response is not None:
+        if 'id' in response:
+          print(f"Video id {response['id']} was successfully uploaded.")
+        else:
+          exit(f"The upload failed with an unexpected response: {response}")
+    except HttpError as e:
+      if e.resp.status in RETRIABLE_STATUS_CODES:
+        error = f"A retriable HTTP error {e.resp.status} occurred:\n{e.content}" 
+      else:
+        raise
+    except RETRIABLE_EXCEPTIONS as e:
+      error = f"A retriable error occurred: {e}"
+
+    if error is not None:
+      print(error)
+      retry += 1
+      if retry > MAX_RETRIES:
+        exit("No longer attempting to retry.")
+
+      max_sleep = 2 ** retry
+      sleep_seconds = random.random() * max_sleep
+      print(f"Sleeping {sleep_seconds} seconds and then retrying...")
+      time.sleep(sleep_seconds)
+
+def upload_to_youtube(filename, title, description, category=27, keywords=[], privacy_status=PrivacyStatus.PRIVATE):
+  if not os.path.exists(filename):
+    print("Please specify a valid filename")
+    return
+  youtube = get_authenticated_service()
+  try:
+    initialize_upload(youtube, filename, title, description, category, keywords, privacy_status)
+  except HttpError as e:
+    print(f"An HTTP error {e.resp.status} occurred:\n{e.content}")
